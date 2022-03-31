9a894c0338d6e5aa1a46e59cccb83943623023e8
yizhou zhou <yizhouzhou@yizhous-MacBook-Pro.local>
2022-03-30 21:13:52 -0500

    html and css

---
+++ css/note.js
@@ -0,0 +1,22 @@
+/* 
+    inline > internal > external
+    id > class > tagname
+    internal id > external id > internal class > external class > internal tagname > external tagname
+    select1 select2
+    select1 > select2
+    select1, select2
+    select1 ~ select2
+
+    box-model, box-sizing, content-box, border-box
+
+    display:block, inline, inline block, flex
+
+    display:none; visibility:hidden
+
+    position: absolute, relative, fixed, static, sticky
+    sticky will be sticked to the border of the screen when you scroll the page
+    fixed will always be located to certain location in the screen
+    static is the default value
+    absolute: element will be positioned relative to the nearest parent who has a non-static position value
+    relative: element will be positioned based on its original location;
+*/
---
+++ html/script.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+/* window.onload= ()=>{
+    const rectElement = document.querySelector(".footer");
+    console.log(rectElement);
+} */
+window.addEventListener("load", ()=>{
+    const rectElement = document.querySelector(".footer");
+    console.log(rectElement);
+})
+
