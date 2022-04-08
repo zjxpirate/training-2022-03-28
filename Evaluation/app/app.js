4e71f3a904ffbf7d36412b70913588593d2e6a13
zjxpirate <j_zhang21@u.pacific.edu>
2022-04-08 16:40:55 -0700

    second commit by 5pm

--- Evaluation/app/app.js
+++ Evaluation/app/app.js
@@ -10,33 +10,41 @@ async function loadingJSON() {
     for(let i = 0; i < arr.length; i++){
         if(arr[i]["isCompleted"] == false){
             var ul = document.getElementById("pending-list");
-            var li = document.createElement("li");
-            li.appendChild(document.createTextNode(arr[i]["content"]));
-            ul.appendChild(li);
+            console.log(arr[i]["id"])
+            var pendingRow = "<li id = '" + arr[i]["id"] + "'>" + arr[i]["content"] + "<button>Right</button><button onclick='deleteTask()'>Delete</button><button>Edit</button></li>";
+            ul.insertAdjacentHTML('beforeend', pendingRow);
         }else{
             var ul = document.getElementById("completed-list");
-            var li = document.createElement("li");
-            li.appendChild(document.createTextNode(arr[i]["content"]));
-            ul.appendChild(li);
+            var completedRow = "<li id = '" + arr[i]["id"] + "'><button class='left'>Left</button>" + arr[i]["content"] + "<button class='right' onclick='deleteTask()'>Delete</button><button class='right'>Edit</button></li>";
+            ul.insertAdjacentHTML('beforeend', completedRow);
         }
     }
 
+}
+
+
+
+
+function addTask(){
+
+    var inputValue = document.getElementById("inputValue").value;
+
+    var ul = document.getElementById("pending-list");
+    var pendingRow = "<li>" + inputValue + "<button>Right</button><button onclick='deleteTask()'>Delete</button><button>Edit</button></li>";
+    ul.insertAdjacentHTML('beforeend', pendingRow);
 
 
     // add data to JSON 
     let httpPush = new XMLHttpRequest();
     httpPush.open("POST", "http://localhost:3000/todos", true);
     httpPush.setRequestHeader("Content-Type", "application/json");
-    var dataPush = JSON.stringify({ "content": "test", "isCompleted": false, });
+    var dataPush = JSON.stringify({"content": inputValue, "isCompleted": false});
     httpPush.send(dataPush);
-
-
-
-    
-    
 }
 
 
 
 
-
+function deleteTask(){
+    console.log("delete in progress")
+}
