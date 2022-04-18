25583554ef6f09bfe151814f4ce6b118a9bc96f2
zjxpirate <j_zhang21@u.pacific.edu>
2022-04-18 15:18:22 -0700

    here

--- Evaluation/app/app.js
+++ Evaluation/app/app.js
@@ -1,5 +1,8 @@
 
 
+
+
+
 async function loadingJSON() {
 
     // fetch data from JSON
@@ -11,11 +14,11 @@ async function loadingJSON() {
         if(arr[i]["isCompleted"] == false){
             var ul = document.getElementById("pending-list");
             console.log(arr[i]["id"])
-            var pendingRow = "<li id = '" + "rowContent" + arr[i]["id"] + "' >" + arr[i]["content"] + "<button id = '" + "pendingRight" + arr[i]["id"] + "' onclick='rightTask(this.id)'>Right</button><button id = '" + "pendingDelete" + arr[i]["id"] + "' onclick='deleteTask(this.id)'>Delete</button><button id = '" + "pendingEdit" + arr[i]["id"] + "' onclick='editTask(this.id)'>Edit</button></li>";
+            var pendingRow = "<li id = '" + "rowContent" + arr[i]["id"] + "' > <p id='paragraph" + arr[i]["id"] + "' style='display: inline;'>" + arr[i]["content"] + "</p> <form id='form" + arr[i]["id"] + "' style='display: none;'><input id='input" + arr[i]["id"] + "'> <button id='pendingSubmit" + arr[i]["id"] + "' onclick='submitChange(this.id)' style='display:none;'></button> </form> <button id = '" + "pendingRight" + arr[i]["id"] + "' onclick='rightTask(this.id)'>Right</button><button id = '" + "pendingDelete" + arr[i]["id"] + "' onclick='deleteTask(this.id)'>Delete</button><button id = '" + "pendingEdit" + arr[i]["id"] + "' onclick='editTask(this.id)'>Edit</button></li>";
             ul.insertAdjacentHTML('beforeend', pendingRow);
         }else{
             var ul = document.getElementById("completed-list");
-            var completedRow = "<li id = '" + "rowContent" + arr[i]["id"] + "'><button id = '" + "completedLeft" + arr[i]["id"] + "' class='left' onclick='leftTask(this.id)'>Left</button>" + arr[i]["content"] + "<button id = '" + "completedDelete" + arr[i]["id"] + "' class='right' onclick='deleteTask(this.id)'>Delete</button><button id = '" + "completedEdit" + arr[i]["id"] + "' onclick='editTask(this.id)' class='right'>Edit</button></li>";
+            var completedRow = "<li id = '" + "rowContent" + arr[i]["id"] + "'><button id = '" + "completedLeft" + arr[i]["id"] + "' class='left' onclick='leftTask(this.id)'>Left</button> <p id='paragraph" + arr[i]["id"] + "' style='display: inline;'>" + arr[i]["content"] + "</p> <form id='form" + arr[i]["id"] + "' style='display: none;'><input id='input" + arr[i]["id"] + "'><button id='completedSubmit" + arr[i]["id"] + "' onclick='submitChange(this.id)' style='display:none;'></button></form> <button id = '" + "completedDelete" + arr[i]["id"] + "' class='right' onclick='deleteTask(this.id)'>Delete</button><button id = '" + "completedEdit" + arr[i]["id"] + "' onclick='editTask(this.id)' class='right'>Edit</button></li>";
             ul.insertAdjacentHTML('beforeend', completedRow);
         }
     }
@@ -29,9 +32,13 @@ function addTask(){
 
     var inputValue = document.getElementById("inputValue").value;
 
+
+
     var ul = document.getElementById("pending-list");
-    var pendingRow = "<li>" + inputValue + "<button>Right</button><button onclick='deleteTask()'>Delete</button><button>Edit</button></li>";
-    ul.insertAdjacentHTML('beforeend', pendingRow);
+    //var pendingRow = "<li>" + inputValue + "<button>Right</button><button onclick='deleteTask()'>Delete</button><button>Edit</button></li>";
+    
+    var pendingRow = "<li> <p style='display: inline;'>" + inputValue + "</p> <form style='display: none;'><input> <button onclick='submitChange(this.id)' style='display:none;'></button> </form> <button  onclick='rightTask(this.id)'>Right</button><button onclick='deleteTask(this.id)'>Delete</button><button onclick='editTask(this.id)'>Edit</button></li>";
+    ul.insertAdjacentHTML('afterbegin', pendingRow);
 
 
     // add data to JSON 
@@ -41,6 +48,7 @@ function addTask(){
     var dataPush = JSON.stringify({"content": inputValue, "isCompleted": false});
     httpPush.send(dataPush);
 
+    // window.location.reload()
 }
 
 
@@ -62,12 +70,78 @@ function deleteTask(id_clicked){
 }
 
 
+
 function editTask(id_clicked){
     let result = id_clicked.match(/[a-zA-Z]+|[0-9]+/g);
-    console.log(result[1])
+    console.log(result[0]);
+
+
+    if(document.getElementById("paragraph" + result[1]).style.display == "inline"){
+        document.getElementById("paragraph"  + result[1]).style.display = "none";
+        document.getElementById("form"  + result[1]).style.display = "inline";
+    }else{
+        document.getElementById("paragraph"  + result[1]).style.display = "inline";
+        document.getElementById("form"  + result[1]).style.display = "none";
+    }
+    
+
+    var input = document.getElementById("input" + result[1]);
+    input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("submit" + result[1]).click();
+    }
+    });
 
 }
 
+
+function submitChange(id_clicked){
+    let result = id_clicked.match(/[a-zA-Z]+|[0-9]+/g);
+    // alert(result[0]);
+
+    // alert(document.getElementById("input" + result[1]).value)
+    let temp = document.getElementById("input" + result[1]).value
+
+    var url = "http://localhost:3000/todos/" + result[1];
+
+    if(result[0] == "pendingSubmit"){
+        let data = { 
+            isCompleted: false,
+            content: temp,
+            id: result[1]
+        };
+
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            })
+            .then(response => response.json())
+            .then(window.location.reload())
+    }else{
+        let data = { 
+            isCompleted: true,
+            content: temp,
+            id: result[1]
+        };
+
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            })
+            .then(response => response.json())
+            .then(window.location.reload())
+    }
+    
+
+    
+}
 
 
 
@@ -76,8 +150,9 @@ function rightTask(id_clicked){
     let result = id_clicked.match(/[a-zA-Z]+|[0-9]+/g);
     console.log(result[1])
 
-    let originalContent = document.getElementById("rowContent" + result[1]).textContent;
-    originalContent = originalContent.replace("RightDeleteEdit", "");
+    //let originalContent = document.getElementById("rowContent" + result[1]).getElementsByTagName("p").textContent;
+    let originalContent = document.querySelector("#rowContent" + result[1] + " p").textContent;
+    // originalContent = originalContent.replace("RightDeleteEdit", "");
 
     console.log("original content: ", originalContent)
 
@@ -105,17 +180,15 @@ function rightTask(id_clicked){
 
 
 
-
-
-
 function leftTask(id_clicked){
     let result = id_clicked.match(/[a-zA-Z]+|[0-9]+/g);
     console.log(result[1])
 
-    let originalContent = document.getElementById("rowContent" + result[1]).textContent;
-    originalContent = originalContent.replace("Left", "");
-    originalContent = originalContent.replace("Delete", "");
-    originalContent = originalContent.replace("Edit", "");
+    //let originalContent = document.getElementById("rowContent" + result[1]).getElementsByTagName("p").textContent;
+    let originalContent = document.querySelector("#rowContent" + result[1] + " p").textContent;
+    // originalContent = originalContent.replace("Left", "");
+    // originalContent = originalContent.replace("Delete", "");
+    // originalContent = originalContent.replace("Edit", "");
     
     console.log("original content: ", originalContent)
 
